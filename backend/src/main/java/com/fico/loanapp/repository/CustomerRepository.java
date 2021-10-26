2217c0f0b99644ceb3cac947b47f87982be92897
yashgandhe <yash.gandhe@groww.in>
2021-10-26 07:37:06 +0530

    Crud -> JPA 2

--- backend/src/main/java/com/fico/loanapp/repository/CustomerRepository.java
+++ backend/src/main/java/com/fico/loanapp/repository/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.fico.loanapp.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.fico.loanapp.model.Customer;
@@ -10,7 +11,7 @@ import com.fico.loanapp.model.Customer;
 /** This interface implements CRUD functions and provides a method to find customer data using ssn.
  * @param customer object and type of primary key **/
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 	
 	
 	List<Customer> findByssn(int ssn);
